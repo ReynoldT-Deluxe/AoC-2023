892371fa32c1e9252f26552e070df0732cd22649
Reynold Tamayo <54862245+ReynoldT-Deluxe@users.noreply.github.com>
2023-12-05 10:47:29 -0500

    Day 1 and 2 solutions
    created solutions in C# .Net 7

---
+++ Day_1_2023/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Configuration;
+using System.Collections.Specialized;
+using System.Collections;
+
+namespace FileApplication {
+   class Program {
+      static void Main(string[] args) {
+        string dataLocation = "";
+        
+        //data to use
+        //dataLocation = "/Users/t452172/Documents/Personal/Advent_of_Code/2023/AoC-2023/Day_1_2023/sampleData1.txt";
+        //dataLocation = "/Users/t452172/Documents/Personal/Advent_of_Code/2023/AoC-2023/Day_1_2023/sampleData2.txt";
+        //dataLocation = "/Users/t452172/Documents/Personal/Advent_of_Code/2023/AoC-2023/Day_1_2023/aocData.txt"; 
+        dataLocation = "/Users/t452172/Documents/Personal/Advent_of_Code/2023/AoC-2023/Day_1_2023/aocData2.txt"; 
+
+        //problem part
+        //int partNum = 1;
+        int partNum = 2;
+
+            try {
+                using (StreamReader sr = new StreamReader(dataLocation)) {
+                    
+                    string data;
+                    int calibrationTotal = 0;
+                    
+                    while ((data = sr.ReadLine()) != null) {
+                        //check data if it's blank 
+                        if (data.Length != 0) {
+                            calibrationTotal += getCalibrationValue(data, partNum);
+                        }
+                    }
+                    Console.WriteLine("calibrationTotal: {0}", calibrationTotal);
+                }                    
+
+            } catch (Exception e) {
+                // Let the user know what went wrong.
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+      }
+
+      static int getCalibrationValue(string data, int partNum) {
+        int digit = 0;
+        string digitData = "";
+
+        for (int x = data.Length - 1; x >= 0; x--) {
+            string input = data[x].ToString();
+
+            if(partNum.Equals(1)) {                                            
+                if (Int32.TryParse(input, out digit))
+                {
+                    digitData += input;
+                }
+            } else {
+                if (Int32.TryParse(input, out digit)){
+                    digitData += input;
+                } else {
+                    //Console.WriteLine("data[x..]: {0}", data[x..]);
+                    switch (input){
+                        case "o": 
+                            if (data[x..].IndexOf("one").Equals(0)){
+                                digitData += "1";
+                            }
+                            break;
+                        case "t":
+                            if (data[x..].IndexOf("two").Equals(0)){
+                                digitData += "2";
+                            }
+
+                            if (data[x..].IndexOf("three").Equals(0)){
+                                digitData += "3";
+                            }
+                            break;
+                        case "f":
+                            if (data[x..].IndexOf("four").Equals(0)){
+                                digitData += "4";
+                            }
+
+                            if (data[x..].IndexOf("five").Equals(0)){
+                                digitData += "5";
+                            }
+                            break;
+                        case "s":
+                            if (data[x..].IndexOf("six").Equals(0)){
+                                digitData += "6";
+                            }
+
+                            if (data[x..].IndexOf("seven").Equals(0)){
+                                digitData += "7";
+                            }
+                            break;
+                        case "e":
+                            if (data[x..].IndexOf("eight").Equals(0)){
+                                digitData += "8";
+                            }
+                            break;
+                        case "n":
+                            if (data[x..].IndexOf("nine").Equals(0)){
+                                digitData += "9";
+                            }
+                            break;
+                    }
+                }
+            }
+            //Console.WriteLine("digitData: {0}", digitData);
+        }
+
+        //if there is only one digit, use same digit
+        if (digitData.Length.Equals(1)) {
+            digitData += digitData;        
+        }
+
+        //Console.WriteLine("digitData: {0}", digitData);
+
+        //get last and first digit since the order is from the end
+        String output = digitData[digitData.Length - 1].ToString() + digitData[0].ToString();
+
+        //Console.WriteLine("output: {0}", output);
+
+        return Int32.Parse(output);
+      }
+   }
+}
---
+++ Day_2_2023/Program.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Configuration;
+using System.Collections.Specialized;
+using System.Collections;
+
+namespace FileApplication {
+   class Program {
+      static void Main(string[] args) {
+        string dataLocation = "";
+        
+        //data to use
+        //dataLocation = "/Users/t452172/Documents/Personal/Advent_of_Code/2023/AoC-2023/Day_2_2023/sampleData1.txt";
+        dataLocation = "/Users/t452172/Documents/Personal/Advent_of_Code/2023/AoC-2023/Day_2_2023/aocData.txt"; 
+        //dataLocation = "/Users/t452172/Documents/Personal/Advent_of_Code/2023/AoC-2023/Day_2_2023/aocData2.txt"; 
+
+        //problem part
+        //int partNum = 1;
+        int partNum = 2;
+
+            try {
+                using (StreamReader sr = new StreamReader(dataLocation)) {
+                    
+                    string data;
+                    int idSum = 0;
+                    int powerSum = 0;
+
+                    while ((data = sr.ReadLine()) != null) {
+                        //check data if it's blank 
+                        if (data.Length != 0) {
+                            List<string> cubeData = getCubeList(data);
+
+                            if (partNum == 1) {
+                                idSum += getIdOfPossibleGames(data, cubeData);
+                            } else {
+                                powerSum += getPowerOfCubes(data, cubeData);
+                            }
+                        }
+                    }
+
+                    if (partNum == 1) {
+                        Console.WriteLine("idSum: {0}", idSum);
+                    } else {
+                        Console.WriteLine("powerSum: {0}", powerSum);
+                    }
+                }                    
+
+            } catch (Exception e) {
+                // Let the user know what went wrong.
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+      }
+
+      static List<string> getCubeList(string data) {
+
+        //Console.WriteLine("data: {0}", data);
+
+        //get id from data
+        string[] initialData = data.Split(' ');
+        //Console.WriteLine("initialData[1]: {0}", initialData[1]);
+
+        List<string> dataCollectedList = new List<string>();
+        //Console.WriteLine("initialData.Length: {0}", initialData.Length);
+
+        //get cube data
+        for (int x = 2; x < initialData.Length; x++) {
+            string dataCollected = "";
+            //Console.WriteLine("x: {0}", x);
+            //Console.WriteLine("initialData[x+1][0].ToString(): {0}", initialData[x+1][0].ToString());
+            switch (initialData[x+1][0].ToString()) {
+                case "b":
+                    dataCollected = "blue " + initialData[x];
+                    break;
+                case "g":
+                    dataCollected = "green " + initialData[x];
+                    break;
+                case "r":
+                    dataCollected = "red " + initialData[x];
+                    break;
+            }
+            dataCollectedList.Add(dataCollected);
+            
+            if (!x.Equals(initialData.Length)) {
+                x++;
+            }
+        }
+
+        return dataCollectedList;
+      }
+
+      static int getIdOfPossibleGames(string data, List<string> dataCollectedList) {
+
+        int blue = 0;
+        int green = 0;
+        int red = 0;
+
+        foreach(string text in dataCollectedList) {
+            //Console.WriteLine(text);
+            string[] colorData = text.Split((' '));
+            string color = colorData[0];
+
+            if (color.Equals("blue")) {
+                int testBlue = Int32.Parse(colorData[1]);
+                if (testBlue > blue) {
+                    blue = testBlue;
+                }
+            } else if (color.Equals("green")) {
+                int testGreen = Int32.Parse(colorData[1]);
+                if (testGreen > green) {
+                    green = testGreen;
+                }
+            } else if (color.Equals("red")) {
+                int testRed = Int32.Parse(colorData[1]);
+                if (testRed > red) {
+                    red = testRed;
+                }
+            }
+        }
+
+        //Console.WriteLine("blue: {0}", blue);
+        //Console.WriteLine("green: {0}", green);
+        //Console.WriteLine("red: {0}", red);
+
+        int id;
+        string[] initialData = data.Split(' ');
+        //Console.WriteLine("initialData[1]: {0}", initialData[1]);
+        string[] game = initialData[1].Split(':');
+        //Console.WriteLine("game[0]: {0}", game[0]);
+
+        //compare with reference data
+        //red 12
+        //green 13
+        //blue 14
+        if (blue <= 14) {
+            if (green <= 13) {
+                if (red <= 12) {
+                    id = Int32.Parse(game[0]);
+                } else {
+                    id = 0;
+                }
+            } else {
+                id = 0;
+            }
+        } else {
+            id = 0;
+        }
+
+        //Console.WriteLine(id);
+
+        return id;
+      }
+
+      static int getPowerOfCubes(string data, List<string> dataCollectedList) {
+
+        int blue = 0;
+        int green = 0;
+        int red = 0;
+
+        foreach(string text in dataCollectedList) {
+            //Console.WriteLine(text);
+            string[] colorData = text.Split((' '));
+            string color = colorData[0];
+
+            if (color.Equals("blue")) {
+                int testBlue = Int32.Parse(colorData[1]);
+                if (testBlue > blue) {
+                    blue = testBlue;
+                }
+            } else if (color.Equals("green")) {
+                int testGreen = Int32.Parse(colorData[1]);
+                if (testGreen > green) {
+                    green = testGreen;
+                }
+            } else if (color.Equals("red")) {
+                int testRed = Int32.Parse(colorData[1]);
+                if (testRed > red) {
+                    red = testRed;
+                }
+            }
+        }
+
+        //Console.WriteLine("blue: {0}", blue);
+        //Console.WriteLine("green: {0}", green);
+        //Console.WriteLine("red: {0}", red);
+
+       return (blue * green * red);
+      }
+   }
+}
